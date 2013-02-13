3bf6f25866a785f01fd2b9043f28f80b93a6f10c
Charles Ruhland <cruhland@alumni.cmu.edu>
2013-02-12 23:44:18 -0800

    Refactored field assignments into method calls.

--- scratchpad.js
+++ scratchpad.js
@@ -1,14 +1,56 @@
 // Put a solid black square of fixed size on the screen.
 
-function drawSquare() {
+function createDiv() {
     var div = document.createElement('div');
-    div.style.position = 'absolute';
-    div.style.backgroundColor = 'black';
-    div.style.width = '100px';
-    div.style.height = '100px';
-    div.style.left = '250px';
-    div.style.top = '250px';
-    document.body.appendChild(div);
+
+    function setPosition(value) {
+        div.style.position = value;
+    }
+
+    function setBackgroundColor(value) {
+        div.style.backgroundColor = value;
+    }
+
+    function setWidth(value) {
+        div.style.width = value;
+    }
+
+    function setHeight(value) {
+        div.style.height = value;
+    }
+
+    function setLeft(value) {
+        div.style.left = value;
+    }
+
+    function setTop(value) {
+        div.style.top = value;
+    }
+
+    function draw() {
+        document.body.appendChild(div);
+    }
+
+    return {
+        'setPosition': setPosition,
+        'setBackgroundColor': setBackgroundColor,
+        'setWidth': setWidth,
+        'setHeight': setHeight,
+        'setLeft': setLeft,
+        'setTop': setTop,
+        'draw': draw
+    };
+}
+
+function drawSquare() {
+    var div = createDiv();
+    div.setPosition('absolute');
+    div.setBackgroundColor('black');
+    div.setWidth('100px');
+    div.setHeight('100px');
+    div.setLeft('250px');
+    div.setTop('250px');
+    div.draw();
 }
 
 drawSquare();
