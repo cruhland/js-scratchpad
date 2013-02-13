92955b0066fdf958deb9b698a2f2f8224d5e9f05
Charles Ruhland <cruhland@alumni.cmu.edu>
2013-02-12 23:59:40 -0800

    Put all Javascript into a module.

--- scratchpad.js
+++ scratchpad.js
@@ -1,68 +1,72 @@
-// Put a solid black square of fixed size on the screen.
 
-function px(value) {
-    return value + 'px';
-}
+var Scratchpad = (function() {
 
-function createDiv() {
-    var div = document.createElement('div');
-
-    function setPosition(value) {
-        div.style.position = value;
+    function px(value) {
+        return value + 'px';
     }
 
-    function setBackgroundColor(value) {
-        div.style.backgroundColor = value;
+    function createDiv() {
+        var div = document.createElement('div');
+
+        function setPosition(value) {
+            div.style.position = value;
+        }
+
+        function setBackgroundColor(value) {
+            div.style.backgroundColor = value;
+        }
+
+        function setWidth(pixels) {
+            div.style.width = px(pixels);
+        }
+
+        function setHeight(pixels) {
+            div.style.height = px(pixels);
+        }
+
+        function setLeft(pixels) {
+            div.style.left = px(pixels);
+        }
+
+        function setTop(pixels) {
+            div.style.top = px(pixels);
+        }
+
+        function draw() {
+            document.body.appendChild(div);
+        }
+
+        return {
+            'setPosition': setPosition,
+            'setBackgroundColor': setBackgroundColor,
+            'setWidth': setWidth,
+            'setHeight': setHeight,
+            'setLeft': setLeft,
+            'setTop': setTop,
+            'draw': draw
+        };
     }
 
-    function setWidth(pixels) {
-        div.style.width = px(pixels);
-    }
+    var Position = {
+        'Absolute': 'absolute'
+    };
 
-    function setHeight(pixels) {
-        div.style.height = px(pixels);
-    }
+    var Color = {
+        'Black': 'black'
+    };
 
-    function setLeft(pixels) {
-        div.style.left = px(pixels);
-    }
-
-    function setTop(pixels) {
-        div.style.top = px(pixels);
-    }
-
-    function draw() {
-        document.body.appendChild(div);
+    function drawSquare() {
+        var div = createDiv();
+        div.setPosition(Position.Absolute);
+        div.setBackgroundColor(Color.Black);
+        div.setWidth(100);
+        div.setHeight(100);
+        div.setLeft(250);
+        div.setTop(250);
+        div.draw();
     }
 
     return {
-        'setPosition': setPosition,
-        'setBackgroundColor': setBackgroundColor,
-        'setWidth': setWidth,
-        'setHeight': setHeight,
-        'setLeft': setLeft,
-        'setTop': setTop,
-        'draw': draw
+        'drawSquare': drawSquare
     };
-}
-
-var Position = {
-    'Absolute': 'absolute'
-};
-
-var Color = {
-    'Black': 'black'
-};
-
-function drawSquare() {
-    var div = createDiv();
-    div.setPosition(Position.Absolute);
-    div.setBackgroundColor(Color.Black);
-    div.setWidth(100);
-    div.setHeight(100);
-    div.setLeft(250);
-    div.setTop(250);
-    div.draw();
-}
-
-drawSquare();
+})();
